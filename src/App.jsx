a135dfc6a82f1f16511619f0f874e5afcaf31c16
mohitjaint <imohitjaint@gmail.com>
2025-07-17 18:58:49 +0530

    Make Card for About Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,25 @@
 import { useState } from 'react'
-import './App.css'
+import Header from './components/Header/Header'
+import profilePhoto from './assets/profile.jpg'
+import About from './components/Cards/About'
 
 function App() {
-  const [activeButton, setActiveButton] = useState(null)
+
   
   
   return (
    <>
-    <div className='bg-[color:#1A0B2E] font-sans text-white text-left    flex  h-[80px] items-center'>
-    <p className='flex-[1.5] text-[30px] p-4'>&lt; Mohit &gt;</p> 
-
-    <div className='flex justify-between flex-[2] text-[20px] p-4'>
-      <button 
-      key={'about'}
-      onClick={()=>setActiveButton('about')}
-      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
-          ${activeButton === 'about' 
-          ? 'bg-gray-100 text-[#1A0B2E] ' 
-          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
-          `}
-      >About</button>
-      <button
-      key={'cp'}
-      onClick={()=>setActiveButton('cp')}
-      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
-          ${activeButton === 'cp' 
-          ? 'bg-gray-100 text-[#1A0B2E] ' 
-          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
-          `}
-      >Competitive Programming</button>
-
-      <button
-      key={'projects'}
-      onClick={()=>setActiveButton('projects')}
-      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
-          ${activeButton === 'projects' 
-          ? 'bg-gray-100 text-[#1A0B2E] ' 
-          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
-          `}
-      >Projects</button>
-      <button
-      key={'connect'}
-      onClick={()=>setActiveButton('connect')}
-      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
-          ${activeButton === 'connect' 
-          ? 'bg-gray-100 text-[#1A0B2E] ' 
-          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
-          `}
-      >Connect</button>
+    <Header />
+    <div className="w-[570px] h-[190px] bg-[linear-gradient(to_right,_#130428_7%,_#251043_34%,_#38126D_57%,_#261045_85%,_#190634_100%)] rounded-xl mt-16">
     </div>
-    </div>
+    <div className="w-[570px] h-[190px] rounded-xl mt-6 bg-[linear-gradient(to_bottom_right,_#130428_7%,_#251043_34%,_#38126D_57%,_#261045_85%,_#190634_100%)]"></div>
+
+
+
+
     
+
+
    </>
 
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+function Header() {
+  const [activeButton, setActiveButton] = useState(null)
+  
+  
+  return (
+    <div className='bg-[color:#1A0B2E] font-sans text-white text-left    flex  h-[80px] items-center'>
+    <p className='flex-[1.5] text-[30px] p-4'>&lt; Mohit &gt;</p> 
+
+    <div className='flex justify-between flex-[2] text-[20px] p-4'>
+      <button 
+      key={'about'}
+      onClick={()=>setActiveButton('about')}
+      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
+          ${activeButton === 'about' 
+          ? 'bg-gray-100 text-[#1A0B2E] font-semibold' 
+          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
+          `}
+      >About</button>
+      <button
+      key={'cp'}
+      onClick={()=>setActiveButton('cp')}
+      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
+          ${activeButton === 'cp' 
+          ? 'bg-gray-100 text-[#1A0B2E] font-semibold' 
+          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
+          `}
+      >Competitive Programming</button>
+
+      <button
+      key={'projects'}
+      onClick={()=>setActiveButton('projects')}
+      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
+          ${activeButton === 'projects' 
+          ? 'bg-gray-100 text-[#1A0B2E] font-semibold' 
+          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
+          `}
+      >Projects</button>
+      <button
+      key={'connect'}
+      onClick={()=>setActiveButton('connect')}
+      className={`p-2 rounded-xl delay-100    transition-all duration-300 ease-in-out 
+          ${activeButton === 'connect' 
+          ? 'bg-gray-100 text-[#1A0B2E] font-semibold' 
+          : 'text-white hover:shadow-[0_0_10px_#ffffff]'}
+          `}
+      >Connect</button>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Header
