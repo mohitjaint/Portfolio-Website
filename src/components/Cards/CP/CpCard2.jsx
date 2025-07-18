67351a9087b8fc6687a23a2edcb462ec31c99dc3
mohitjaint <imohitjaint@gmail.com>
2025-07-18 19:11:03 +0530

    Change CP section card style

--- src/components/Cards/CP/CpCard2.jsx
+++ src/components/Cards/CP/CpCard2.jsx
@@ -6,9 +6,12 @@ function CpCard2() {
       <div className="relative w-full h-70 rounded-xl mt-6 mb-10 shadow-sm shadow-[#693B93]">
         <div className="absolute inset-0 z-0 rounded-xl bg-gradient-to-br from-[#2A0845] via-[#3B0D5F] to-[#1F053A]"></div>
         <div className="relative z-10 flex flex-col gap-6 items-start justify-center h-full px-6 pt-4">
-          <div className="w-full p-4 rounded-lg bg-[#1A0B2E] text-white text-xl">Codeforces</div>
-          <div className="w-full p-4 rounded-lg bg-[#1A0B2E] text-white text-xl">Codeforces</div>
-          <div className="w-full p-4 rounded-lg bg-[#1A0B2E] text-white text-xl">Codeforces</div>
+          <div className="w-full p-4 rounded-lg bg-[linear-gradient(to_right,_rgba(105,59,147,0.2)_0%,_rgba(110,191,244,0.2)_77%,_rgba(70,144,212,0.2)_100%)] backdrop-blur-md
+ text-white text-xl">Codeforces</div>
+          <div className="w-full p-4 rounded-lg bg-[linear-gradient(to_right,_rgba(105,59,147,0.2)_0%,_rgba(110,191,244,0.2)_77%,_rgba(70,144,212,0.2)_100%)] backdrop-blur-md
+ text-white text-xl">Codeforces</div>
+          <div className="w-full p-4 rounded-lg bg-[linear-gradient(to_right,_rgba(105,59,147,0.2)_0%,_rgba(110,191,244,0.2)_77%,_rgba(70,144,212,0.2)_100%)] backdrop-blur-md
+ text-white text-xl">Codeforces</div>
         </div>
       </div>
     </div>
